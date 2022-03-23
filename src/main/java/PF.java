7bf742c64ea93c2f00baba81dcc8d832b94032ee
Core <zergi89@mail.ru>
2022-03-23 17:36:35 +0300

    Change

--- src/main/java/PF.java
+++ src/main/java/PF.java
@@ -1,5 +1,14 @@
 public class PF {
     public static void main(String[] args) {
         System.out.println("Smile this World");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+
+
+        System.out.println(1.0 / 2);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");
     }
 }
