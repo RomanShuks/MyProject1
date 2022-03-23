42ae2c92afd59f5792a2c28b53f68b5cf3452f8d
Core <zergi89@mail.ru>
2022-03-23 23:54:28 +0300

    Change

--- src/main/java/PF.java
+++ src/main/java/PF.java
@@ -4,26 +4,27 @@ public class PF {
         hello("FCK");
         hello("Night");
 
+        Square s = new Square();
+        s.l = 5;
+
         double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-
+        Rectangle r = new Rectangle();
         double a = 4;
         double b = 6;
-        System.out.println("Площадь прмоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        System.out.println("Площадь прмоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String soso) {
         System.out.println("Smile, " + soso + "!");
     }
 
-    public static double area(double len) {
-        return len * len;
+    public static double area(double Square s) {
+        return s.l  * s.l;
     }
 
-public static double area(double a, double b) {
-        return a * b;
+    public static double area(Rectangle r) {
+        return r.a * r.b;
+    }
 }
-
-
-   }
