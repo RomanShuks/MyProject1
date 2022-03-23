ff4f06806abec2f17f15fb8a076f1781a27f48a0
Core <zergi89@mail.ru>
2022-03-23 17:50:29 +0300

    Change

---
+++ src/main/java/HUAhua.java
@@ -0,0 +1,10 @@
+public class HUAhua {
+    public static void main(String[] args) {
+        System.out.println("Hello, " + "world!");
+
+
+        int l = 8;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + (l * l));
+
+    }
+}
