46ec6c4c413a6d5a97aa9f56032e4dd8b32e9560
Core <zergi89@mail.ru>
2022-03-23 18:21:47 +0300

    Change

--- src/main/java/HUAhua.java
+++ src/main/java/HUAhua.java
@@ -2,7 +2,6 @@ public class HUAhua {
     public static void main(String[] args) {
         String soso = "world";
         System.out.println("Hello, " + soso + "!");
-
         double l = 8.0;
         double s = l * l;
         System.out.println("Площадь квадрата со стороной " + l + " = " + s);
