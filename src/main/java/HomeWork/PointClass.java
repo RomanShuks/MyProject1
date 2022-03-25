953e517b8bfa79c62422bfd81a825c1900f2a104
Core <zergi89@mail.ru>
2022-03-25 11:47:23 +0300

    v3

--- src/main/java/HomeWork/PointClass.java
+++ src/main/java/HomeWork/PointClass.java
@@ -5,12 +5,10 @@ public class PointClass {
     public static void main(String[] args) {
         Point p1 = new Point(6, 7);
         Point p2 = new Point(4, 8);
-        PointCalculator calculator = new PointCalculator();
+        PointCalculator calculator =  new PointCalculator();
         System.out.println("Расстояние между точками равно " + calculator.distance(p1, p2));
         System.out.println("Расстояние между точками равно " + staticDistance(p1, p2));
-
     }
-
     public static double staticDistance(Point p1, Point p2) {
         return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     }
